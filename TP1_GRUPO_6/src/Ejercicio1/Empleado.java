c6e85dc071036f842261d5557d0acebcd71d314a
facumarcati <facumarcati14@gmail.com>
2023-03-29 16:30:03 -0300

    Variables privadas, setters y getters y variable para generar los ID automaticamente

--- TP1_GRUPO_6/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_6/src/Ejercicio1/Empleado.java
@@ -7,4 +7,28 @@ public class Empleado
 	private int edad;
 	
 	static int cont=1000;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
