db704482cc8d17ee5e3eb04169d252539938f978
ribeiromauri <110779735+ribeiromauri@users.noreply.github.com>
2023-03-29 22:28:02 -0300

    avance punto 5

--- TP1_GRUPO_6/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_6/src/Ejercicio1/Empleado.java
@@ -9,6 +9,15 @@ public class Empleado
 	public Empleado() {
 		cont++;
 		this.id = cont;
+		this.nombre = "Sin nombre";
+		this.edad = 99;
+	}
+	
+	public Empleado(String nombre, int edad) {
+		cont++;
+		this.id = cont;
+		this.nombre = nombre;
+		this.edad = edad;
 	}
 
 	public int getId() {
