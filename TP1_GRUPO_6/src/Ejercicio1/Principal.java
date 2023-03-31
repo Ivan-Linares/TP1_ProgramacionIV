c9cb9093b4ae1f3901ea48ca031a0581200a6433
facumarcati <facumarcati14@gmail.com>
2023-03-31 17:06:46 -0300

    Empleado creado con el constructor vacio

--- TP1_GRUPO_6/src/Ejercicio1/Principal.java
+++ TP1_GRUPO_6/src/Ejercicio1/Principal.java
@@ -3,18 +3,19 @@ package Ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Empleado Tuke = new Empleado();
 		Empleado Teto = new Empleado("Teto", 41);
+		Empleado Tato = new Empleado();
 		
 		Tuke.setNombre("Tuke");
 		
-		System.out.println("ID: " + Tuke.getId() + " Nombre: " + Tuke.getNombre());
+		//System.out.println("ID: " + Tuke.getId() + " Nombre: " + Tuke.getNombre());
 		System.out.println(Tuke.toString());
 		System.out.println(Teto.toString());
+		System.out.println(Tato.toString());
 		
-		System.out.println(Empleado.devuelveProximoID());
+		System.out.println("ID del siguiente empleado: " + Empleado.devuelveProximoID());
 	}
 
 }
